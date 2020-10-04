6a76a5bb0cd7f17d8ce5f3b514cec2808c8f311c
dannymckinney88 <dannymckinney88@gmail.com>
2020-10-04 08:47:39 -0700

    Didn't start my repo and commits right away. I am starting to add the hit and stand funcionality

--- blackjack/js/app.js
+++ blackjack/js/app.js
@@ -1,13 +1,31 @@
 // Selectors 
     // card elements 
-const playerCard = document.querySelectorAll('.player-card')
-const cpuCard = document.querySelectorAll('.cpu-card')
-const playerContainer = document.querySelector('.card-container-player')
-const cpuContainer =  document.querySelector('.card-container-cpu')
+    const playerContainer = document.querySelector('.card-container-player')
+    const cpuContainer =  document.querySelector('.card-container-cpu')
     //buttons
-const dealButton = document.querySelector('.deal')
-const hitButton = document.querySelector('.hit')
-const standButton = document.querySelector('.stand')
+    const dealButton = document.querySelector('.deal')
+    const hitButton = document.querySelector('.hit')
+    const standButton = document.querySelector('.stand')
+    //creating and setting up cards
+    let playerCard = document.createElement('img')
+    let playerCards = [document.createElement('img'), document.createElement('img')]
+    let cpuCard = document.querySelectorAll('.cpu-card')
+    let cpuCards = [document.createElement('img'),document.createElement('img')]
+    for(let i =0; i < playerCards.length; i++){
+        playerCards[i].setAttribute('class','player-card')
+        playerCards[i].setAttribute('src','./assests/blackjack/cardback1.png' )
+        cpuCards[i].setAttribute('class','cpu-card')
+        cpuCards[i].setAttribute('src','./assests/blackjack/cardback1.png' )
+        console.log(playerCards[i],cpuCards[i])
+
+    }
+console.log(playerCards)
+playerCard.setAttribute('class','player-card')
+playerCard.setAttribute('src','./assests/blackjack/cardback1.png' )
+playerContainer.appendChild(playerCard)
+
+
+
 
 const tempDiv = document.createElement('div')
 let tempCard = document.createElement('img')
@@ -17,6 +35,8 @@ let tempCards = []
 let game = {
     playerCards: [],
     cpuCards: [],
+    playerTurn: true,
+    cpuTurn: false,
     
     deck: [],
     // Builds a full 52card deck and puts them in the deck array.
@@ -41,7 +61,28 @@ let game = {
     },
 
     hit() {
-        
+        hitButton.addEventListener('click', function(){
+            if(game.playerTurn){
+            playerCard = game.deck[0]
+            playerCard.setAttribute('class', 'cpu-card')
+            playerContainer.appendChild(playerCard)
+            game.deck.shift()
+            console.log(game.cpuTurn)
+            }else if (game.cpuTurn){
+                cpuCard = game.deck[0]
+                cpuCard.setAttribute('class', 'cpu-card')
+                cpuContainer.appendChild(cpuCard)
+                game.deck.shift()
+            }
+        })
+    },
+
+    stand(){
+        standButton.addEventListener('click', function(){
+            game.playerTurn = false
+            game.cpuTurn = true
+            console.log(`cpu: ${game.cpuTurn}`)
+        })
     }
 }
 // Creates cards with suit. Suits are labeled (s =spade c=clubs h=heart d=dimond)
@@ -72,13 +113,12 @@ function makeCards(suit){
     }
 }
 
-// Shuffels deck 
-
 
 game.buildDeck()
 game.shuffleDeck(game.deck)
-for(let i=0; i < 52; i++){
-    console.log(game.deck[i])
-}
-
-
+game.hit()
+// game.hit()
+// for(let i=0; i < 52; i++){
+//     console.log(game.deck[i])
+// }
+game.stand()
