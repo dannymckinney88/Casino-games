c5a0dd6239abe996dc8a43652639225997fbe903
dannymckinney88 <dannymckinney88@gmail.com>
2020-10-06 18:57:22 -0700

    Re-wrote code added classes

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,7 @@
 // Selectors 
     // card elements 
 const playerContainer = document.querySelector('.card-container-player')
-const cpuContainer =  document.querySelector('.card-container-cpu')
+const dealerContainer =  document.querySelector('.card-container-dealer')
     //buttons
 const dealButton = document.querySelector('.deal-btn')
 const hitButton = document.querySelector('.hit')
@@ -25,6 +25,7 @@ class Casino  {
     constructor(name,playerCredit){
         this.name = name
         this.playerCredit = playerCredit
+        this.deck = []
     }
     // Creates cards with suit. Suits are labeled (s =spade c=clubs h=heart d=dimond)
     buildCards(suit){
@@ -34,7 +35,7 @@ class Casino  {
                 const card = document.createElement('img');
                 card.setAttribute('value',i)
                 card.setAttribute('src',`./assests/blackjack/${i}${suit}.png`);
-                game.deck.push(card)
+                this.deck.push(card)
                 console.log()
             }
             else if(i > 10 && i < 14){ 
@@ -42,13 +43,13 @@ class Casino  {
                 const card = document.createElement('img');
                 card.setAttribute('value',`10`);
                 card.setAttribute('src',`./assests/blackjack/${i}${suit}.png`);
-                game.deck.push(card)
+                this.deck.push(card)
             } else { 
                 // builds the Ace and sets its value to 11
                 const card = document.createElement('img');
                 card.setAttribute('value',`11`);
                 card.setAttribute('src',`./assests/blackjack/${i}${suit}.png`);
-                game.deck.push(card)
+                this.deck.push(card)
             }
             
             
@@ -76,117 +77,70 @@ class Casino  {
 }
 
 class BlackJack extends Casino {
-
-}
-
-// Game object
-let game = {
-    playerCards: [],
-    cpuCards: [],
-    playerTurn: false,
-    cpuTurn: false,
-    playerTotal: 0,
-    cpuTotal: 0,
-    firstDeal: true,
-    playerHasAce: false,
-    cpuHasAce: false,
-    cpuQualify: false,
-    
-    deck: [],
-    // Creates cards with suit. Suits are labeled (s =spade c=clubs h=heart d=dimond)
-    buildCards(suit){
-        for(let i=2; i < 15; i++){
-            // Builds cards 2-10 and sets their value
-            if(i < 11){
-                const card = document.createElement('img');
-                card.setAttribute('value',i)
-                card.setAttribute('src',`./assests/blackjack/${i}${suit}.png`);
-                game.deck.push(card)
-                console.log()
-            }
-            else if(i > 10 && i < 14){ 
-                //Builds cards J, Q, K and set their value to 10
-                const card = document.createElement('img');
-                card.setAttribute('value',`10`);
-                card.setAttribute('src',`./assests/blackjack/${i}${suit}.png`);
-                game.deck.push(card)
-            } else { 
-                // builds the Ace and sets its value to 11
-                const card = document.createElement('img');
-                card.setAttribute('value',`11`);
-                card.setAttribute('src',`./assests/blackjack/${i}${suit}.png`);
-                game.deck.push(card)
-            }
-            
-            
+    constructor(name,playerCredit){  
+        super(name,playerCredit) ; {
+        this.playerCard = ''
+        this.dealerCard = ''
+        this.playerCards = []
+        this.dealerCards = []
+        this.playerTurn = false
+        this.dealerTurn = false
+        this.playerTotal = 0
+        this.dealerTotal = 0
+        this.playerHasAce = false
+        this.dealerHadAce = false
+        this.firstDeal = false
+        this.dealerQualify = false
         }
-    },
+    }
     
-    // Builds a full 52card deck and puts them in the deck array.
-    buildDeck(suit){
-        this.buildCards('s')
-        this.buildCards('c')
-        this.buildCards('h')
-        this.buildCards('d')
-
-    },
-    // Shuffles the deck of cards 
-    shuffleDeck(deck) {
-        for(let i = deck.length -1; i > 0; i--){
-            //Shuffling around each index to make sure random everytime.
-            const j = Math.floor(Math.random() * i)
-            const temp = deck[i]
-            deck[i] = deck[j]
-            deck[j] = temp
-        }
-    },
     // Deals the game out and runs some checks for blackjack 
     deal() {
         //setting intiall values and resetting board
         modal.style.display = "none"
         this.firstDeal = true
         playerContainer.innerHTML = ''
-        cpuContainer.innerHTML = ''
+        dealerContainer.innerHTML = ''
         this.playerCards = []
-        this.cpuCards = []
+        this.dealerCards = []
         this.bet()
-        // dealing cards out switching from player to cpu for visuals
+        // dealing cards out switching from player to dealer for visuals
         for(let i =0; i < 2; i++){
                 // comme back and figure out how to slow down speed of cards comming out----
-            playerCard = this.deck[0]
-            playerCard.setAttribute('class', 'player-card')
-            playerContainer.appendChild(playerCard)
-            this.playerCards.push(playerCard)
+            this.playerCard = this.deck[0]
+            this.playerCard.setAttribute('class', 'player-card')
+            playerContainer.appendChild(this.playerCard)
+            this.playerCards.push(this.playerCard)
             this.deck.shift()
             this.playerTurn = false
-            this.cpuTurn = true
-            cpuCard = this.deck[0]
-            cpuCard.setAttribute('class', 'cpu-card')
-            cpuContainer.appendChild(cpuCard)
-            this.cpuCards.push(cpuCard)
+            this.dealerTurn = true
+            this.dealerCard = this.deck[0]
+            this.dealerCard.setAttribute('class', 'dealer-card')
+            dealerContainer.appendChild(this.dealerCard)
+            this.dealerCards.push(this.dealerCard)
             this.deck.shift()
         }
         //Reseting values 
-        this.cpuHasAce = false
+        this.dealerHasAce = false
         this.playerHasAce = false
         this.playerTurn = true
-        this.cpuTurn = false
+        this.dealerTurn = false
         hitButton.disabled = false
         // Checking for black Jack
         this.checkForAceAndtotal()
         this.checkBlackJack()
-    },
+    }
         
     // Tracks your money
     bet() {
         player.money -= 5
         console.log('test'+ player.money)
         playerMoney.innerHTML = '$'+player.money
-    },
+    }
     // Checks for blackjac after intialll deal
     checkBlackJack(){
         //Checking if either both or just one player has black jack
-        if(this.playerTotal==21 && this.cpuTotal==21){
+        if(this.playerTotal==21 && this.dealerTotal==21){
             hitButton.disabled = true
             winnerText.innerHTML = "You push"
             modal.style.display = "block"
@@ -196,52 +150,52 @@ let game = {
             winnerText.innerHTML = "Winner Winner Chicken Dinner"
             modal.style.display = "block"
         }
-        else if(this.cpuTotal == 21){
+        else if(this.dealerTotal == 21){
             hitButton.disabled = true
             winnerText.innerHTML = "You Dealer Wins"
             modal.style.display = "block"
         }
-    },
+    }
     //Lets the player and dealer hit
     hit() {
         //Checks which players turn it is and then gives card to correct player
        if(this.playerTurn){
         //Creates a card for the player and adds to to the playerCards array
-        playerCard = this.deck[0]
-        playerCard.setAttribute('class', 'player-card')
-        playerContainer.appendChild(playerCard)
-        this.playerCards.push(playerCard)
+        this.playerCard = this.deck[0]
+        this.playerCard.setAttribute('class', 'player-card')
+        playerContainer.appendChild(this.playerCard)
+        this.playerCards.push(this.playerCard)
         this.deck.shift()
         this.checkForAceAndtotal()
         this.HasAce()
         this.checkForWin()
-      }else if (this.cpuTurn){
-        //Creates a card for the dealer and adds to to the cpuCards array
-        cpuCard = this.deck[0]
-        cpuCard.setAttribute('class', 'cpu-card')
-        cpuContainer.appendChild(cpuCard)
-        this.cpuCards.push(cpuCard)
+      }else if (this.dealerTurn){
+        //Creates a card for the dealer and adds to to the dealerCards array
+        this.dealerCard = this.deck[0]
+        this.dealerCard.setAttribute('class', 'dealer-card')
+        dealerContainer.appendChild(this.dealerCard)
+        this.dealerCards.push(this.dealerCard)
         this.deck.shift()
         this.checkForAceAndtotal()
         this.HasAce()
         this.checkForWin()
         
        }
-    },
+    }
     // Stops players turn and switches to dealer.
     stand(){
         this.playerTurn = false
-        this.cpuTurn = true
+        this.dealerTurn = true
         this.checkForAceAndtotal()
         this.HasAce()
         this.dealerHit()
         this.checkForWin()
         
-    },
+    }
     // Checks to see if either player has an Ace and counts card total
     checkForAceAndtotal() {
             this.playerTotal = 0
-            this.cpuTotal = 0
+            this.dealerTotal = 0
             //  Checks to see if either player has an ace
             for(let i= 0; i < this.playerCards.length; i++){
                 this.playerTotal += parseInt(this.playerCards[i].getAttribute('value'))
@@ -250,13 +204,13 @@ let game = {
                    
                 }
             }
-            for(let i= 0; i< this.cpuCards.length; i ++){
-                this.cpuTotal += parseInt(this.cpuCards[i].getAttribute('value'))
-                if(parseInt(this.cpuCards[i].getAttribute('value')) ===11){
-                    this.cpuHasAce = true
+            for(let i= 0; i< this.dealerCards.length; i ++){
+                this.dealerTotal += parseInt(this.dealerCards[i].getAttribute('value'))
+                if(parseInt(this.dealerCards[i].getAttribute('value')) ===11){
+                    this.dealerHasAce = true
                 }
             }
-    },
+    }
     //  After getting totall checks logic for ace cards if any are out
     HasAce(){
         //    Makes sure that an ace can either be 11 or 1
@@ -272,54 +226,54 @@ let game = {
                         console.log(this.playerCards[i].getAttribute('value'))
                     }
                 }
-                if(this.cpuTotal >= 22){
+                if(this.dealerTotal >= 22){
                    this.playerBusted = true
                 }
             }
         }
 
-        if(this.cpuHasAce){
+        if(this.dealerHasAce){
             // makking sure ace works for dealer
-            if(this.cpuTotal >= 22){
-                console.log('Dealer has ace:'+this.cpuHasAce)
-                this.cpuTotal -= 10
-                this.cpuHasAce = false
-                console.log('Dealer has ace after:'+this.cpuHasAce)
-                for(let i=0; i< this.cpuCards.length; i++){
-                    if(parseInt(this.cpuCards[i].getAttribute('value')) ===11){
-                        this.cpuCards[i].setAttribute('value', 1)
+            if(this.dealerTotal >= 22){
+                console.log('Dealer has ace:'+this.dealerHasAce)
+                this.dealerTotal -= 10
+                this.dealerHasAce = false
+                console.log('Dealer has ace after:'+this.dealerHasAce)
+                for(let i=0; i< this.dealerCards.length; i++){
+                    if(parseInt(this.dealerCards[i].getAttribute('value')) ===11){
+                        this.dealerCards[i].setAttribute('value', 1)
                     }
                 }
-                if(this.cpuTotal >= 22){
+                if(this.dealerTotal >= 22){
                     hitButton.disabled = true
                 }
             }
         }
         
-    },
+    }
     // If player stands this has dealer player by itself
     dealerHit(){
-        if(this.cpuTurn){
+        if(this.dealerTurn){
             console.log('test')
-            while(this.cpuTotal < 17){
+            while(this.dealerTotal < 17){
                     console.log('I need to hit')
                     this.hit()
                     this.checkForAceAndtotal()
                     //Checks if dealer bust
-                    if(this.cpuTotal <= 21 && this.cpuTotal >=17){
-                        this.cpuQualify = true;
-                        console.log(this.cpuTotal, this.cpuQualify)
+                    if(this.dealerTotal <= 21 && this.dealerTotal >=17){
+                        this.dealerQualify = true;
+                        console.log(this.dealerTotal, this.dealerQualify)
                     }
                     this.checkForWin()
                 // Checking to see if dealer needs to hit
             }
-            if(this.cpuTotal <= 21 && this.cpuTotal >=17){
-                this.cpuQualify = true;
-                console.log(this.cpuTotal, this.cpuQualify)
+            if(this.dealerTotal <= 21 && this.dealerTotal >=17){
+                this.dealerQualify = true;
+                console.log(this.dealerTotal, this.dealerQualify)
             }
             this.checkForWin()
     }
-},
+}
     //  Checks all win condtions 
     checkForWin(){
             //  Checks for Ace and makes sure it counts as 1 or 11
@@ -329,14 +283,14 @@ let game = {
                 winnerText.innerHTML = "You bust"
                 modal.style.display = "block"         
             }
-                if(this.cpuTotal > 21){
-                    console.log(this.cpuTotal)
+                if(this.dealerTotal > 21){
+                    console.log(this.dealerTotal)
                     hitButton.disabled = true
                     winnerText.innerHTML = "Dealer bust"
                     modal.style.display = "block"
                 }
-                if(this.cpuQualify){
-                    if(this.playerTotal > this.cpuTotal){
+                if(this.dealerQualify){
+                    if(this.playerTotal > this.dealerTotal){
                         hitButton.disabled = true
                         winnerText.innerHTML = "YOU WIN!!!!!"
                         modal.style.display = "block"
@@ -345,33 +299,35 @@ let game = {
                         winnerText.innerHTML = "The house wins"
                         modal.style.display = "block"
                     }
-                    this.cpuQualify = false;
+                    this.dealerQualify = false;
                 }
        }
-            
-       
     }
+
+let blackJack = new BlackJack("Danny's Casino", 1000)
+console.log(blackJack)
+
     //Sets up decks
-game.buildDeck()
-game.shuffleDeck(game.deck)
-game.checkForWin()
+blackJack.buildDeck()
+blackJack.shuffleDeck(blackJack.deck)
+blackJack.checkForWin()
      
 // Event listeners
 dealButton.addEventListener('click', ()=>{
-        game.deal()
+        blackJack.deal()
     
 
     
 })
 
 hitButton.addEventListener('click', ()=>{
-    game.hit()
-    game.checkForWin()
+    blackJack.hit()
+    blackJack.checkForWin()
     // console.log('Player total: '+ game.playerTotal)
 })
 
 standButton.addEventListener('click', ()=>{
-    game.stand()
+    blackJack.stand()
 })
 
 
@@ -386,3 +342,5 @@ span.onclick = function() {
 //     modal.style.display = "none";
 //   }
 // }
+
+
