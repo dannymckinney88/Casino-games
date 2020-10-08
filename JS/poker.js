266df48a75389603c23dd8fabaa372c75c4c1ac3
dannymckinney88 <dannymckinney88@gmail.com>
2020-10-07 18:42:10 -0700

    Finshing touches but logic is all done

--- JS/poker.js
+++ JS/poker.js
@@ -1,8 +1,40 @@
+//Checking for win condtions 
+// get all values from value attribute, loop through and check each element against each other
+// add the value and then push value into new array
+// then check how many times each time pairs, 3x and 4x show up
+//Check for suits adding up same way 
+// Then check if the numbers are consecutive
+// Check highest 1st,  royal flush,straight flush, 4x, full house, flush, straight, 3x, 2 pair
+//--Royal flush 
+    //check ids for suit and value card value then check if all 5 cards are same id(suiut) and  consecutive order (10-14 sort the values)
+// --straight flush same as about with any order use previous number - 1 = %0
+//-- 4 of a kind
+    // check if 4 values are the same 
+//--fullHouse
+    //Check if 3 values are the same and then if 2 values are the same. Use some counter and check if total is 5 then true
+//--Flush
+    // check if all 5 are the same value if so true 
+// -- straight 
+    // Check if all 5 numbers are consecutive
+// -- 3 of a kind
+    //  check if 3 values are in the array
+//  2 pair 
+    //  Check if there are 2 diffrent numbers consecutive
+
+// Selectors 
 const pokerCards = document.querySelectorAll('.card')
 const pokerCardContainer = document.querySelectorAll('.card-container')
 const cardsToDiscard = document.querySelectorAll('input')
 const dealBtn = document.querySelector('.deal-btn')
-console.log(pokerCardContainer)
+//Event listeners
+dealBtn.addEventListener('click', function(){
+    poker.discard()
+    console.log(poker.flushCounter,poker.pairsCounter,poker.threeOfAkindCounter)
+  
+         
+  })
+
+
 
 class Casino  {
     constructor(name){
@@ -54,7 +86,10 @@ class Poker extends Casino {
             this.fullHouse = false
             this.flush = false
             this.straight = false
+            this.threeOfAKind = false
             this.twoPair = false
+           
+            
         }
     }
     deal(){
@@ -67,8 +102,81 @@ class Poker extends Casino {
         }
     }
     discard(){
-        
+        for(let i=0;i<5;i++){
+            //    console.log(cardsToDiscard[i])
+            
+            if(!cardsToDiscard[i].checked){
+                poker.deck.shift()
+                // console.log(poker.deck[1])
+                pokerCardContainer[i].innerHTML = ''
+                // console.log(pokerCardContainer[i])
+                pokerCardContainer[i].appendChild(poker.deck[1])
+                // console.log('I am comming from poker deck'+this.deck[i])
+    
+                // console.log(temp)
+                poker.playerHand.splice(i,1,poker.deck[1])
+                poker.deck.shift()
+                // cardsToDiscard[i].checked = false
+            }
+        } 
     }
+    pairsAndFlush(){
+        //Temp vars
+        let totals = []
+        let flushArray = []
+        pairsCounter = 0
+        threeOfAkindCounter
+        fourOfAkindCounter = 0
+        flushCounter = 0
+
+        for(let i = 0; i< poker.playerHand.length; i++){
+            let pairs= 0
+            let suits= 0
+            for(let j = 0; j< poker.playerHand.length; j++){
+                if(parseInt(poker.playerHand[i].getAttribute('value')) === parseInt(poker.playerHand[j].getAttribute('value'))){
+                    pairs ++
+                }
+                if(poker.playerHand[i].getAttribute('id') === poker.playerHand[j].getAttribute('id') ){
+                    suits++
+                }
+                // console.log(parseInt(poker.playerHand[i].getAttribute('value')))
+            }
+                totals.push(pairs)
+                flushArray.push(suits)
+                pairs = 0
+                suits = 0
+        } 
+            console.log(totals)
+            console.log(flushArray)
+        for(let i=0; i < totals.length; i++){
+            if(totals[i] === 2){
+                pairsCounter++
+            }
+            if(totals[i] === 3){
+                threeOfAkindCounter++
+            }
+            if(totals[i] === 4){
+                fourOfAkindCounter++
+            }
+            if(flushArray[i] === 5){
+                flushCounter++
+            }
+        }
+        if(fourOfAkindCounter ===4){
+            this.forOfAKind = true
+        }
+        else if(threeOfAkindCounter ===3 && pairsCounter ===2){
+            this.fullHouse = true
+        }
+        else if(threeOfAkindCounter === 3){
+            this.threeOfAKind = true
+        }else if(pairsCounter ===4){
+            this.twoPair = true
+        }
+   }
+   hasStraight(){
+
+   }
 }
 
 let poker = new Poker('name')
@@ -79,7 +187,7 @@ poker.buildDeck()
 // console.log(poker.deck)
 poker.shuffleDeck()
 poker.deal()
-
+poker.pairsAndFlush()
 function test () {
     for(let i=0; i<5;i++){
         console.log(poker.playerHand[i])
@@ -97,94 +205,8 @@ function test () {
 // console.log(dealBtn)
 //Listening for checked values and getting that id back
 function discard() {
-    for(let i=0;i<5;i++){
-        //    console.log(cardsToDiscard[i])
-        
-        if(!cardsToDiscard[i].checked){
-            poker.deck.shift()
-            // console.log(poker.deck[1])
-            pokerCardContainer[i].innerHTML = ''
-            // console.log(pokerCardContainer[i])
-            pokerCardContainer[i].appendChild(poker.deck[1])
-            // console.log('I am comming from poker deck'+this.deck[i])
-
-            // console.log(temp)
-            poker.playerHand.splice(i,1,poker.deck[1])
-            poker.deck.shift()
-            // cardsToDiscard[i].checked = false
-        }
-    } 
+  
 }
-dealBtn.addEventListener('click', function(){
-  discard()
-   
-test()
-       
-})
-
-//Checking for win condtions 
-// get all values from img attribute, loop through and check each element against each other
-// add each index and save value into new array
-// then check how many times each number sequence appears
-// Check highest 1st,  royal flush,straight flush, 4x, full house, flush, straight, 3x, 2 pair
-//--Royal flush 
-    //check ids for suit and value card value then check if all 5 cards are same id(suiut) and  consecutive order (10-14 sort the values)
-// --straight flush same as about with any order use previous number - 1 = %0
-//-- 4 of a kind
-    // check if 4 values are the same 
-//--fullHouse
-    //Check if 3 values are the same and then if 2 values are the same. Use some counter and check if total is 5 then true
-//--Flush
-    // check if all 5 are the same value if so true 
-// -- straight 
-    // Check if all 5 numbers are consecutive
-// -- 3 of a kind
-    //  check if 3 values are in the array
-//  2 pair 
-    //  Check if there are 2 diffrent numbers consecutive
-
-
-
-let totals = []
-let flushArray = []
-
-for(let i = 0; i< poker.playerHand.length; i++){
-    let temp= 0
-    let temp2= 0
-    for(let j = 0; j< poker.playerHand.length; j++){
-        if(parseInt(poker.playerHand[i].getAttribute('value')) === parseInt(poker.playerHand[j].getAttribute('value'))){
-            temp ++
-        }
-        if(poker.playerHand[i].getAttribute('value') === poker.playerHand[j].getAttribute('value') ){
-            temp2++
-        }
-        // console.log(parseInt(poker.playerHand[i].getAttribute('value')))
-    }
-    flushArray.push(temp2)
-    totals.push(temp)
-    temp = 0
-    
-}
-let pairs;
-let threeOfAKind;
-let fourOfAKind;
-let flush;
-console.log(flushArray)
-for(let i=0; i < totals.length; i++){
-    if(totals[i] === 2){
-        pairs++
-    }
-    if(totals[i] === 3){
-        threeOfAKind++
-    }
-    if(totals[i] === 4){
-        forOfAKind++
-    }
-    if(flushArray[i] === 5){
-        flush++
-    }
-}
-console.log(totals)
 let isStraight = false
 function isConsecutive (max,min){
     if(max - min +1 ===5){
@@ -192,5 +214,5 @@ function isConsecutive (max,min){
     }
 }
 
-isConsecutive(6,2)
+isConsecutive(5,1)
 console.log(isStraight)
